8226e4a69433e782a80296a3c251d9218d250f2a
carol-cymk <13750173@me.mcu.edu.tw>
2026-04-30 15:52:22 +0800

    Add files via upload

---
+++ week10/week10-2a.py
@@ -0,0 +1,23 @@
+# week10-2a.py 學習計畫 Binary Search Tree - DFS
+# DFS 深度優先搜尋 tree 最喜歡用「函式呼叫函式」來解
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        def helper(root):
+            if root == None:
+                return 0  # 沒有節點，深度是0
+
+            left = helper(root.left)    # 左子樹深度
+            right = helper(root.right)  # 右子樹深度
+
+            return max(left, right) + 1  # 取較大 + 自己這層
+
+        return helper(root)
